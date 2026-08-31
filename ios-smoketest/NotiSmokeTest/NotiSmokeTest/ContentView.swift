902defb77ad7ef292defb3f36fa87bd5adc1d915
Vi Nguyen <vixinhxinh@Vis-MacBook-Air.local>
2026-08-31 18:18:05 +0700

    Add NotiSmokeTest Xcode project

---
+++ ios-smoketest/NotiSmokeTest/NotiSmokeTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotiSmokeTest
+//
+//  Created by Vi Nguyen on 31/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-smoketest/NotiSmokeTest/NotiSmokeTest/NotiSmokeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotiSmokeTestApp.swift
+//  NotiSmokeTest
+//
+//  Created by Vi Nguyen on 31/8/26.
+//
+
+import SwiftUI
+
+@main
+struct NotiSmokeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
